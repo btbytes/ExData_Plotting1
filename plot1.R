0da30544205c70543fb912a731ad23a7a980e4f4
Pradeep Gowda <pradeep@btbytes.com>
2015-08-09 11:00:52 -0400

    Solution to Problem 1 - plot1.R

---
+++ plot1.R
@@ -0,0 +1,8 @@
+# Plot 1
+hpc = read.csv2("household_power_consumption.txt", header=TRUE, sep=";")
+hpcSubset <- hpc[hpc$Date %in% c("1/2/2007","2/2/2007"), ]
+gap <- as.numeric(hpcSubset$Global_active_power)
+png("plot1.png", width=504, height=504)
+hist(gap, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)", ylab="Frequency")
+dev.off()
+
